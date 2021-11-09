96f57d53df3304211a7fdf14a90159a1bf665455
kevcouv <kevcouv@hotmail.com>
2021-11-09 12:59:36 +0100

    clear le cache et refaire une migration

--- migrations/Version20211109115827.php
+++ migrations/Version20211109115827.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210917203919 extends AbstractMigration
+final class Version20211109115827 extends AbstractMigration
 {
     public function getDescription(): string
     {
